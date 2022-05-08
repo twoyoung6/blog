b0042168002fc5675a06c5d071efea25736756b4
yangzhan <yzlz5259@163.com>
2022-05-08 19:03:23 +0800

    初次提交

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,123 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <video id="v1" autoplay loop muted>
+    <source src="./assets/bg_kuxuan.mp4" type="video/mp4" />
+  </video>
+  <a-layout>
+    <a-layout-header>
+      <div class="logo">
+        <img alt="logo" src="./assets/logo.png" />
+        <div class="title"><span>木小羊</span><span>TwoYoung</span></div>
+      </div>
+      <div class="topic">
+        <a href="">主 页</a>
+        <a href="">博 客</a>
+        <a href="">定 格</a>
+        <a href="">壹 句</a>
+        <a href="">阅 读</a>
+        <a href="">关 于</a>
+      </div>
+      <div class="msn">
+        <a href="">GitHub</a>
+        <a href="">公众号</a>
+        <a href="">知乎</a>
+      </div>
+    </a-layout-header>
+    <a-layout-content>Content</a-layout-content>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  video {
+    position: fixed;
+    right: 0px;
+    bottom: 0px;
+    min-width: 100%;
+    min-height: 100%;
+    height: auto;
+    width: auto;
+    /*加滤镜*/
+    // filter: blur(5px); //背景模糊设置
+    // -webkit-filter: grayscale(100%);
+    // filter: grayscale(100%); //背景灰度设置
+    z-index: -11;
+  }
+  source {
+    min-width: 100%;
+    min-height: 100%;
+    height: auto;
+    width: auto;
+  }
+
+  .ant-layout {
+    min-height: 100vh;
+    overflow-x: hidden;
+    background: unset;
+    .ant-layout-header {
+      background: #000;
+      height: unset;
+      padding: 12px 60px;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      line-height: unset;
+      .logo {
+        display: flex;
+        img {
+          width: auto;
+          height: 60px;
+        }
+        .title {
+          padding-left: 10px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          span {
+            color: #fff;
+            font-weight: 600;
+            font-size: 16px;
+            &:nth-child(1) {
+              letter-spacing: 12px;
+            }
+          }
+        }
+      }
+      .topic {
+        display: flex;
+        align-items: center;
+        margin: 0;
+        padding: 0 0 0 100px;
+        a {
+          padding: 0 20px;
+          color: #fff;
+        }
+      }
+      .msn {
+        display: flex;
+        align-items: center;
+        margin: 0;
+        padding: 0 0 0 100px;
+        a {
+          padding: 0 20px;
+          color: #fff;
+        }
+      }
+    }
+    .ant-layout-content {
+      padding: 30px;
+    }
+    .ant-layout-footer {
+      background: rgb(44, 44, 44);
+    }
+  }
 }
 </style>
