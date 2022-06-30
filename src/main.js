718fe119cce2eb3656eb65737702959bae3bce50
Twoyoung <7886903+twoyoung@user.noreply.gitee.com>
2022-06-30 14:16:36 +0800

    config（配置路由）

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue'
 import App from './App'
+import Router from "./router";
 import 'ant-design-vue/dist/antd.css'
 
 const app = createApp(App)
 
-app.use(Antd).mount('#app')
+app.use(Router).use(Antd).mount('#app')
